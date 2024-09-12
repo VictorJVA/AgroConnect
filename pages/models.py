c034bb0dfd9fb1a097460270ac498c6ea746378c
VictorJVA <vjvilladia@eafit.edu.co>
2024-09-12 18:59:13 -0500

    feat: basic home page added

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
