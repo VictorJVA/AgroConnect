c16fce8b08732eca608cb59f4e494d3419bde85f
Camilo Cordoba <ccordobab@eafit.edu.co>
2024-09-14 17:13:43 -0500

    feat: Farmer & Post models added

--- pages/models.py
+++ pages/models.py
@@ -15,3 +15,22 @@ class Driver(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)    
+
+class Farmer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  
+    country = models.TextField(max_length=50)
+    state = models.TextField(max_length=50)
+    postal_code = models.IntegerField()
+
+class Post(models.Model):
+    farmer = models.ForeignKey(Farmer, on_delete=models.CASCADE)
+    name = models.TextField(max_length=50)
+    stock = models.IntegerField()
+    delivery_date = models.DateTimeField()
+    description = models.TextField()
+    fare = models.IntegerField()
+    arrival_date = models.DateTimeField()
+    Origin = models.TextField(max_length=50)
+    Destination = models.TextField(max_length=50)
