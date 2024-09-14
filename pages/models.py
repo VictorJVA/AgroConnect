55d291c116882e7422ec9d219ba9f5b00faedb71
VictorJVA <vjvilladia@eafit.edu.co>
2024-09-14 16:20:32 -0500

    feat: User & Driver model added

--- pages/models.py
+++ pages/models.py
@@ -7,13 +7,11 @@ class User(models.Model):
     email = models.EmailField(max_length=50)
     password = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    country = models.CharField(max_length=50)
-    postal_code = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.name
+class Driver(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    
