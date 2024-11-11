9aca592e157e58105ad92a4c40dcf512ddded46b
camilocb2004 <camilocb2004@gmail.com>
2024-11-11 15:22:59 -0500

    feat:implementacion de dos idiomas parcialmente lista

--- agroconnect/settings.py
+++ agroconnect/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -131,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),  # Cambia 'locale' si deseas otra ubicación
+]
